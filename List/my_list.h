ed682a3e0cd824f19d42373a7c34a6c30967546e
sentiurinaDasha <sentdasha106@gmail.com>
2026-02-23 20:39:19 +0300

    лабораторная работа 2: двусвязный список

---
+++ List/my_list.h
@@ -0,0 +1,526 @@
+﻿#pragma once  // защита от повторного включения
+#include <initializer_list>  
+#include <memory>          
+#include <iterator>        
+#include <algorithm>       
+#include <stdexcept>       
+#include <utility>          
+
+template<typename T>
+class MyList
+{
+private:
+   
+    struct Node {
+        T data;     
+        Node* prev; 
+        Node* next; 
+
+        Node(const T& value) : data(value), prev(nullptr), next(nullptr) {}  
+        Node(T&& value) : data(std::move(value)), prev(nullptr), next(nullptr) {}  // конструктор с перемещением
+    };
+
+    Node* head;   // указатель на первый элемент
+    Node* tail;   // указатель на последний элемент
+    size_t count; // количество элементов
+
+    // инициализация пустого списка
+    void init() {
+        head = nullptr;
+        tail = nullptr;
+        count = 0;
+    }
+
+public:
+    // стандартные типы для совместимости со stl
+    using value_type = T;
+    using size_type = size_t;
+    using difference_type = ptrdiff_t;
+    using pointer = value_type*;
+    using const_pointer = const value_type*;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+
+    // итератор 
+    class iterator
+    {
+    private:
+        Node* ptr;          // текущий узел
+        Node* list_tail;    // хвост списка 
+    public:
+        using iterator_category = std::bidirectional_iterator_tag;
+        using value_type = T;
+        using difference_type = ptrdiff_t;
+        using pointer = T*;
+        using reference = T&;
+
+        iterator(Node* p = nullptr, Node* t = nullptr) : ptr(p), list_tail(t) {}
+
+        bool operator==(const iterator& other) const { return ptr == other.ptr; }
+        bool operator!=(const iterator& other) const { return ptr != other.ptr; }
+        reference operator*() { return ptr->data; }
+        pointer operator->() { return &(ptr->data); }
+
+        // префиксный инкремент
+        iterator& operator++() {
+            if (ptr) ptr = ptr->next;
+            return *this;
+        }
+
+        // постфиксный инкремент
+        iterator operator++(int) {
+            iterator tmp = *this;
+            if (ptr) ptr = ptr->next;
+            return tmp;
+        }
+
+        // префиксный декремент
+        iterator& operator--() {
+            if (ptr && ptr->prev) {
+                ptr = ptr->prev;
+            }
+            else if (!ptr && list_tail) {
+                // если это end(), то -- должен вернуть последний элемент
+                ptr = list_tail;
+            }
+            return *this;
+        }
+
+        // постфиксный декремент
+        iterator operator--(int) {
+            iterator tmp = *this;
+            if (ptr && ptr->prev) {
+                ptr = ptr->prev;
+            }
+            else if (!ptr && list_tail) {
+                ptr = list_tail;
+            }
+            return tmp;
+        }
+
+        Node* node() const { return ptr; }
+    };
+
+    //  константный итератор
+    class const_iterator
+    {
+    private:
+        const Node* ptr;
+        const Node* list_tail;
+    public:
+        using iterator_category = std::bidirectional_iterator_tag;
+        using value_type = T;
+        using difference_type = ptrdiff_t;
+        using pointer = const T*;
+        using reference = const T&;
+
+        const_iterator(const Node* p = nullptr, const Node* t = nullptr) : ptr(p), list_tail(t) {}
+        const_iterator(const iterator& it) : ptr(it.node()), list_tail(nullptr) {}
+
+        bool operator==(const const_iterator& other) const { return ptr == other.ptr; }
+        bool operator!=(const const_iterator& other) const { return ptr != other.ptr; }
+        const_reference operator*() const { return ptr->data; }
+
+        const_iterator& operator++() {
+            if (ptr) ptr = ptr->next;
+            return *this;
+        }
+
+        const_iterator operator++(int) {
+            const_iterator tmp = *this;
+            if (ptr) ptr = ptr->next;
+            return tmp;
+        }
+
+        const_iterator& operator--() {
+            if (ptr && ptr->prev) {
+                ptr = ptr->prev;
+            }
+            else if (!ptr && list_tail) {
+                ptr = list_tail;
+            }
+            return *this;
+        }
+
+        const_iterator operator--(int) {
+            const_iterator tmp = *this;
+            if (ptr && ptr->prev) {
+                ptr = ptr->prev;
+            }
+            else if (!ptr && list_tail) {
+                ptr = list_tail;
+            }
+            return tmp;
+        }
+    };
+
+    // типы для обратных итераторов
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+    //конструкторы
+    MyList() { init(); }  // конструктор по умолчанию
+
+    // конструктор копирования
+    MyList(const MyList& other) : MyList() {
+        for (const auto& elem : other) {
+            push_back(elem);
+        }
+    }
+
+    // конструктор от initializer_list {1, 2, 3}
+    MyList(std::initializer_list<T> init) : MyList() {
+        for (const auto& elem : init) {
+            push_back(elem);
+        }
+    }
+
+    // перемещающий конструктор
+    MyList(MyList&& other) noexcept
+        : head(other.head), tail(other.tail), count(other.count) {
+        other.head = nullptr;
+        other.tail = nullptr;
+        other.count = 0;
+    }
+
+    //деструктор 
+    ~MyList() { clear(); }
+
+    //  операторы присваивания 
+    MyList& operator=(const MyList& other) {
+        if (this != &other) {
+            clear();
+            for (const auto& elem : other) {
+                push_back(elem);
+            }
+        }
+        return *this;
+    }
+
+    MyList& operator=(MyList&& other) noexcept {
+        if (this != &other) {
+            clear();
+            head = other.head;
+            tail = other.tail;
+            count = other.count;
+            other.head = nullptr;
+            other.tail = nullptr;
+            other.count = 0;
+        }
+        return *this;
+    }
+
+    MyList& operator=(std::initializer_list<T> ilist) {
+        clear();
+        for (const auto& elem : ilist) {
+            push_back(elem);
+        }
+        return *this;
+    }
+
+    // доступ к элементам
+    reference front() { return head->data; }
+    const_reference front() const { return head->data; }
+    reference back() { return tail->data; }
+    const_reference back() const { return tail->data; }
+
+    //  итераторы 
+    iterator begin() noexcept { return iterator(head, tail); }
+    iterator end() noexcept { return iterator(nullptr, tail); }
+    const_iterator begin() const noexcept { return const_iterator(head, tail); }
+    const_iterator end() const noexcept { return const_iterator(nullptr, tail); }
+    const_iterator cbegin() const noexcept { return const_iterator(head, tail); }
+    const_iterator cend() const noexcept { return const_iterator(nullptr, tail); }
+    reverse_iterator rbegin() noexcept { return reverse_iterator(end()); }
+    reverse_iterator rend() noexcept { return reverse_iterator(begin()); }
+    const_reverse_iterator rbegin() const noexcept { return const_reverse_iterator(end()); }
+    const_reverse_iterator rend() const noexcept { return const_reverse_iterator(begin()); }
+    const_reverse_iterator crbegin() const noexcept { return const_reverse_iterator(end()); }
+    const_reverse_iterator crend() const noexcept { return const_reverse_iterator(begin()); }
+
+    // вместимость
+    bool empty() const noexcept { return count == 0; }
+    size_type size() const noexcept { return count; }
+
+    //  модификаторы 
+    void clear() noexcept {
+        while (head) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+        tail = nullptr;
+        count = 0;
+    }
+
+    void push_back(const T& value) {
+        Node* newNode = new Node(value);
+        if (!head) {
+            head = tail = newNode;
+        }
+        else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+        ++count;
+    }
+
+    void push_front(const T& value) {
+        Node* newNode = new Node(value);
+        if (!head) {
+            head = tail = newNode;
+        }
+        else {
+            head->prev = newNode;
+            newNode->next = head;
+            head = newNode;
+        }
+        ++count;
+    }
+
+    void pop_back() {
+        if (empty()) return;
+        Node* temp = tail;
+        tail = tail->prev;
+        if (tail) {
+            tail->next = nullptr;
+        }
+        else {
+            head = nullptr;
+        }
+        delete temp;
+        --count;
+    }
+
+    void pop_front() {
+        if (empty()) return;
+        Node* temp = head;
+        head = head->next;
+        if (head) {
+            head->prev = nullptr;
+        }
+        else {
+            tail = nullptr;
+        }
+        delete temp;
+        --count;
+    }
+
+    iterator insert(iterator pos, const T& value) {
+        Node* newNode = new Node(value);
+        Node* current = pos.node();
+
+        if (!current) { // вставка в конец
+            if (!head) {
+                head = tail = newNode;
+            }
+            else {
+                tail->next = newNode;
+                newNode->prev = tail;
+                tail = newNode;
+            }
+        }
+        else if (current == head) { // вставка в начало
+            newNode->next = head;
+            if (head) head->prev = newNode;
+            head = newNode;
+        }
+        else { // вставка в середину
+            newNode->prev = current->prev;
+            newNode->next = current;
+            if (current->prev) current->prev->next = newNode;
+            current->prev = newNode;
+        }
+        ++count;
+        return iterator(newNode, tail);
+    }
+
+    iterator erase(iterator pos) {
+        Node* current = pos.node();
+        if (!current) return end();
+
+        Node* nextNode = current->next;
+
+        if (current->prev) {
+            current->prev->next = current->next;
+        }
+        else {
+            head = current->next;
+        }
+
+        if (current->next) {
+            current->next->prev = current->prev;
+        }
+        else {
+            tail = current->prev;
+        }
+
+        delete current;
+        --count;
+        return iterator(nextNode, tail);
+    }
+
+    iterator erase(iterator first, iterator last) {
+        while (first != last) {
+            first = erase(first);
+        }
+        return last;
+    }
+
+    // задание 2.4 развернуть список 
+    void reverse() noexcept {
+        if (count < 2) return;
+
+        Node* current = head;
+        Node* temp = nullptr;
+
+        while (current) {
+            temp = current->prev;
+            current->prev = current->next;
+            current->next = temp;
+            current = current->prev;
+        }
+
+        temp = head;
+        head = tail;
+        tail = temp;
+    }
+
+    //  задание 2.1 подсчет положительных элементов
+    size_type count_positive() const {
+        size_type result = 0;
+        const Node* current = head;
+        while (current) {
+            if (current->data > 0) ++result;
+            current = current->next;
+        }
+        return result;
+    }
+
+    // задание 2.2: найти максимум и сколько раз встречается
+    std::pair<T, size_type> find_max_and_count() const {
+        if (empty()) throw std::out_of_range("List is empty");
+
+        const Node* current = head;
+        T max_elem = current->data;
+        size_type count_max = 1;
+        current = current->next;
+
+        while (current) {
+            if (current->data > max_elem) {
+                max_elem = current->data;
+                count_max = 1;
+            }
+            else if (current->data == max_elem) {
+                ++count_max;
+            }
+            current = current->next;
+        }
+        return { max_elem, count_max };
+    }
+
+    //  задание 2.3: разделить на два списка (без выделения памяти) 
+    void split(MyList<T>& negative, MyList<T>& non_negative) {
+        negative.clear();
+        non_negative.clear();
+
+        if (empty()) return;
+
+        Node* current = head;
+        while (current) {
+            Node* next = current->next;
+
+            // отсоединяем текущий узел от исходного списка
+            if (current->prev) {
+                current->prev->next = current->next;
+            }
+            else {
+                head = current->next;
+            }
+            if (current->next) {
+                current->next->prev = current->prev;
+            }
+            else {
+                tail = current->prev;
+            }
+
+            // обнуляем связи узла
+            current->prev = nullptr;
+            current->next = nullptr;
+
+            // определяем, в какой список добавлять
+            if (current->data < 0) {
+                // добавляем в список отрицательных
+                if (!negative.head) {
+                    negative.head = negative.tail = current;
+                }
+                else {
+                    negative.tail->next = current;
+                    current->prev = negative.tail;
+                    negative.tail = current;
+                }
+                ++negative.count;
+            }
+            else {
+                // добавляем в список неотрицательных
+                if (!non_negative.head) {
+                    non_negative.head = non_negative.tail = current;
+                }
+                else {
+                    non_negative.tail->next = current;
+                    current->prev = non_negative.tail;
+                    non_negative.tail = current;
+                }
+                ++non_negative.count;
+            }
+
+            current = next;
+        }
+
+        // исходный список становится пустым
+        head = tail = nullptr;
+        count = 0;
+    }
+
+    //  универсальные методы с perfect forwarding
+    template<typename U>
+    iterator insert(iterator pos, U&& value) {
+        Node* newNode = new Node(std::forward<U>(value));
+        Node* current = pos.node();
+
+        if (!current) { // вставка в конец
+            if (!head) {
+                head = tail = newNode;
+            }
+            else {
+                tail->next = newNode;
+                newNode->prev = tail;
+                tail = newNode;
+            }
+        }
+        else if (current == head) { // вставка в начало
+            newNode->next = head;
+            if (head) head->prev = newNode;
+            head = newNode;
+        }
+        else { // вставка в середину
+            newNode->prev = current->prev;
+            newNode->next = current;
+            if (current->prev) current->prev->next = newNode;
+            current->prev = newNode;
+        }
+        ++count;
+        return iterator(newNode, tail);
+    }
+
+    template<typename U>
+    void push_back(U&& value) {
+        insert(end(), std::forward<U>(value));
+    }
+
+    template<typename U>
+    void push_front(U&& value) {
+        insert(begin(), std::forward<U>(value));
+    }
+};
---
+++ ListTests/ListTests.cpp
@@ -0,0 +1,211 @@
+﻿#include <list>
+#include <vector>
+#include <string>
+#include "CppUnitTest.h"
+#include "my_list.h"
+
+namespace ListTests { class SimpleElem; }
+namespace ListTests { class NoCopyElem; }
+
+template<typename T> using ContainerTemplate = MyList<T>;
+//template<typename T> using ContainerTemplate = std::list<T>;
+
+// Используем SimpleElem для тестов
+using Elem = ListTests::SimpleElem;
+//using Elem = ListTests::NoCopyElem;
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ListTests
+{
+    class SimpleElem {
+        uint32_t watch;
+        int data;
+        static size_t elem_counter;
+        void check() const { Assert::IsTrue(watch == 0xDEADBEEF, L"Повреждение памяти!! (Обращение к неинициализированному экземпляру класса SimpleElem)"); }
+    public:
+        SimpleElem(const SimpleElem& e) : data(e.data), watch(e.watch) { e.check(); ++elem_counter; }
+        SimpleElem& operator=(const SimpleElem& e) { e.check(); data = e.data; watch = e.watch; return *this; }
+        explicit SimpleElem(int value) : data(value), watch(0xDEADBEEF) { ++elem_counter; }
+        SimpleElem(SimpleElem&& e) noexcept { e.check(); data = e.data; watch = e.watch; ++elem_counter; }
+        SimpleElem& operator=(SimpleElem&& e) noexcept { check(); e.check(); data = e.data; watch = e.watch; return *this; }
+        bool operator==(const SimpleElem& e) const { check(); e.check(); return data == e.data; }
+        ~SimpleElem() { check(); watch = 0; --elem_counter; }
+        static size_t count() { return elem_counter; }
+    };
+    size_t SimpleElem::elem_counter = 0;
+
+    class NoCopyElem {
+        uint32_t watch;
+        int data;
+        static size_t elem_counter;
+        void check() const { Assert::IsTrue(watch == 0xDEADDEAD, L"Повреждение памяти!! (Обращение к неинициализированному экземпляру класса NoCopyElem)"); }
+    public:
+        NoCopyElem(const NoCopyElem& e) = delete;
+        NoCopyElem& operator=(const NoCopyElem& e) = delete;
+        explicit NoCopyElem(int value) : data(value), watch(0xDEADDEAD) { ++elem_counter; }
+        NoCopyElem(NoCopyElem&& e) noexcept { e.check(); data = e.data; watch = e.watch; ++elem_counter; }
+        NoCopyElem& operator=(NoCopyElem&& e) noexcept { check(); e.check(); data = e.data; watch = e.watch; return *this; }
+        bool operator==(const NoCopyElem& e) const { check(); e.check(); return data == e.data; }
+        ~NoCopyElem() { check(); watch = 0; --elem_counter; }
+        static size_t count() { return elem_counter; }
+    };
+    size_t NoCopyElem::elem_counter = 0;
+
+
+    TEST_CLASS(ComplexTypeTests)
+    {
+    public:
+
+        TEST_METHOD(IteratorTests)
+        {
+            ContainerTemplate<int> list{ 3, 4, 8, 5, 2, 13, 20 };
+            list.push_back(10);
+            Assert::IsTrue(list.end() == ++--list.end(), L"++--list.end() должен возвращать list.end()");
+            Assert::AreEqual(10, *(--list.end()), L"--list.end() должен возвращать последний элемент");
+        }
+
+        TEST_METHOD(StringTests)
+        {
+            ContainerTemplate<std::string> list{ "123", "AAAAAAAA", "abc", "cde" };
+            std::vector<std::string> expected1{ "123", "AAAAAAAA", "abc", "cde" };
+
+            Assert::AreEqual(size_t(4), list.size(), L"Неправильно считается количество строковых элементов");
+            Assert::IsTrue(std::equal(list.begin(), list.end(), expected1.begin(), expected1.end()), L"Неправильный порядок строковых элементов");
+            Assert::IsTrue(std::equal(list.rbegin(), list.rend(), expected1.rbegin(), expected1.rend()), L"Неправильный порядок строковых элементов");
+
+            for (const auto& str : expected1)
+                list.erase(std::find(list.begin(), list.end(), str));
+
+            Assert::AreEqual(size_t(0), list.size(), L"Список не пуст после удаления всех элементов");
+            for (const auto& elem : list)
+                Assert::Fail(L"Что-то лежит в списке после удаления всех элементов");
+
+            list.push_back("test-1");
+            list.push_back("test-2");
+            list.push_back("test-4");
+            std::vector<std::string> expected2{ "test-1", "test-2", "test-4" };
+            Assert::IsTrue(std::equal(list.begin(), list.end(), expected2.begin(), expected2.end()), L"Неправильный порядок строковых элементов");
+            Assert::IsTrue(std::equal(list.rbegin(), list.rend(), expected2.rbegin(), expected2.rend()), L"Неправильный порядок строковых элементов");
+        }
+
+        TEST_METHOD(ElemTests)
+        {
+            size_t init_count = Elem::count();
+            {
+                ContainerTemplate<Elem> list;
+                Assert::AreEqual(size_t(0), Elem::count() - init_count, L"Создан лишний экземпляр класса Elem");
+
+                for (const auto& elem : list)
+                    Assert::Fail(L"Что-то лежит в пустом списке");
+
+                list.push_back(Elem(40));
+                list.push_back(Elem(75));
+                list.push_front(Elem(50));
+                list.push_back(Elem(23));
+                list.push_back(Elem(87));
+                Assert::IsTrue(Elem::count() - init_count == 5, L"Создан лишний экземпляр класса Elem");
+
+                auto find_result = std::find(list.begin(), list.end(), Elem(75));
+                Assert::IsTrue(find_result != list.end(), L"Не найден элемент со значением 75");
+                Assert::IsTrue(*find_result == Elem(75), L"Неправильно найден элемент со значением 75");
+
+                list.erase(find_result);
+                Assert::IsTrue(Elem::count() - init_count == 4, L"Неправильно работает удаление элементов списка");
+            }
+            Assert::IsTrue(Elem::count() - init_count == 0, L"Утечка памяти!!");
+        }
+
+        TEST_METHOD(CopyElemTests)
+        {
+            size_t init_count = Elem::count();
+            {
+                ContainerTemplate<Elem> list;
+                {
+                    ContainerTemplate<Elem> list1;
+                    ContainerTemplate<Elem> list2(std::move(list1));
+                    for (const auto& elem : list2)
+                        Assert::Fail(L"Что-то лежит в пустом списке после копирования");
+
+                    list2.push_back(Elem(40));
+                    list2.push_front(Elem(50));
+                    list2.push_back(Elem(23));
+                    list2.push_back(Elem(87));
+                    Assert::IsTrue(Elem::count() - init_count == 4, L"Неправильно работает удаление элементов списка");
+                    list = std::move(list2);
+                }
+                Assert::IsTrue(list.back() == Elem(87), L"Некправильно работает копирование списка");
+                Assert::IsTrue(Elem::count() - init_count == 4, L"Неправильно работает удаление элементов списка");
+            }
+            Assert::IsTrue(Elem::count() - init_count == 0, L"Утечка памяти!!");
+        }
+
+        TEST_METHOD(CountPositiveTests)
+        {
+            ContainerTemplate<int> list{ -3, 5, -1, 8, 0, -7, 2, 4 };
+            Assert::AreEqual(size_t(4), list.count_positive(), L"Неправильно подсчитаны положительные элементы");
+
+            ContainerTemplate<int> empty_list;
+            Assert::AreEqual(size_t(0), empty_list.count_positive(), L"В пустом списке должно быть 0 положительных");
+
+            ContainerTemplate<int> all_negative{ -1, -2, -3 };
+            Assert::AreEqual(size_t(0), all_negative.count_positive(), L"В списке с отрицательными должно быть 0 положительных");
+        }
+
+        TEST_METHOD(FindMaxAndCountTests)
+        {
+            ContainerTemplate<int> list{ 3, 7, 2, 7, 5, 7, 1 };
+            auto [max_val, count] = list.find_max_and_count();
+            Assert::AreEqual(7, max_val, L"Неправильно найден максимальный элемент");
+            Assert::AreEqual(size_t(3), count, L"Неправильно подсчитано количество вхождений максимума");
+
+            ContainerTemplate<int> list2{ 5 };
+            auto [max_val2, count2] = list2.find_max_and_count();
+            Assert::AreEqual(5, max_val2, L"Неправильно найден максимум в списке из одного элемента");
+            Assert::AreEqual(size_t(1), count2, L"Неправильно подсчитано количество в списке из одного элемента");
+
+            auto test_empty = [&]() {
+                ContainerTemplate<int> empty_list;
+                auto result = empty_list.find_max_and_count();
+                };
+            Assert::ExpectException<std::out_of_range>(test_empty, L"Должно быть исключение для пустого списка");
+        }
+
+        TEST_METHOD(SplitTests)
+        {
+            ContainerTemplate<int> list{ -5, 3, -2, 7, 0, -1, 4, -8 };
+            ContainerTemplate<int> negative, non_negative;
+
+            list.split(negative, non_negative);
+
+            Assert::AreEqual(size_t(0), list.size(), L"Исходный список должен быть пуст после split");
+            Assert::AreEqual(size_t(4), negative.size(), L"Неправильное количество отрицательных");
+            Assert::AreEqual(size_t(4), non_negative.size(), L"Неправильное количество неотрицательных");
+
+            for (const auto& val : negative) {
+                Assert::IsTrue(val < 0, L"В negative попал неотрицательный элемент");
+            }
+
+            for (const auto& val : non_negative) {
+                Assert::IsTrue(val >= 0, L"В non_negative попал отрицательный элемент");
+            }
+        }
+
+        TEST_METHOD(ReverseTests)
+        {
+            ContainerTemplate<int> list{ 1, 2, 3, 4, 5 };
+            list.reverse();
+
+            std::vector<int> expected{ 5, 4, 3, 2, 1 };
+            Assert::IsTrue(std::equal(list.begin(), list.end(), expected.begin()),
+                L"Неправильно развернут список");
+
+            ContainerTemplate<int> single{ 42 };
+            single.reverse();
+            Assert::AreEqual(42, single.front(), L"reverse с одним элементом должен работать");
+
+            ContainerTemplate<int> empty;
+            empty.reverse();
+        }
+    };
+}
